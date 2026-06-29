97a7996fc5f5253f740eadd0a13bee77d83dd128
Lexie Wambui <wlexie123@gmail.com>
2026-06-30 00:12:23 +0300

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+        <link rel="icon" href="/logo.png" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           rel="preconnect"
